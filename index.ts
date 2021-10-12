5a2e916794d703c43d8c055ac6e986082bf947a1
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-10-12 13:07:15 -0300

    Tratando a tag input

--- index.ts
+++ index.ts
@@ -7,7 +7,6 @@ function soma(a: number, b: number) {
 }
 
 soma('a', 'b')
-*/
 
 // interfaces
 interface IAnimal {
@@ -50,3 +49,12 @@ const felino: IFelino = {
 }
 // Você cria um contrato quando estiver criando seus objetos ou classes.
 */
+
+// Para o TypeScript saber que o getElementById('input') não é um elemento genérico da página, mas é especificamente um input
+const input = document.getElementById('input') as HTMLInputElement; //  as HTMLInputElement está dizendo pro typescrip que é um input
+
+input.addEventListener('input', (event) => { // event é o evento que acontece quando está digitando alguma coisa no meu input
+    // Dizendo pro typescript que esse event está ocorrendo no lemento de input
+    const i = event.currentTarget as HTMLInputElement
+    console.log(i.value)
+})
